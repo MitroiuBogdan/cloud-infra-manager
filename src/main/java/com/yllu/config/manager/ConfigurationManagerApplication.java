e456c002d19e0019c87c260bab00e5b7978f90ad
bogdan.mitroiu <bogdan.mitroiu@stud.etti.upb.ro>
2022-06-18 19:50:03 +0300

    initial setup

---
+++ src/main/java/com/yllu/config/manager/ConfigurationManagerApplication.java
@@ -0,0 +1,17 @@
+package com.yllu.config.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigurationManagerApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationManagerApplication.class, args);
+    }
+
+
+}
